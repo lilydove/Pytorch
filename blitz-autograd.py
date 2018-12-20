f30f53c4b83bacc173a2763ebf5255a8a864021e
Yee Wu <wuyiwangyi@163.com>
2018-12-20 13:30:38 +0800

    12=20

--- blitz-autograd.py
+++ blitz-autograd.py
@@ -4,3 +4,41 @@ import torch
 
 x = torch.ones(2, 2, requires_grad=True)
 print(x)
+
+y = x + 2
+print(y)
+print(y.grad_fn)
+
+z = y * y * 3
+out = z.mean()
+print("z:   ", z, "\nout: ", out)
+
+a = torch.randn(2, 2)
+a = ((a * 3))/(a - 1)
+print(a.requires_grad)
+a.requires_grad_(True)
+print(a.requires_grad)
+b = (a * a).sum()
+print(b.grad_fn)
+
+out.backward()
+print(x.grad)
+
+# 3 dimentions
+x = torch.randn(3, requires_grad=True)
+print(x)
+y = x * 2
+while y.data.norm() < 800:
+    y = y * 2
+print(y)
+
+# x1=.2 x2=1.0 x3=.00001 they are inserting values.
+v = torch.tensor([.2, 1.0, 0.0001], dtype=torch.float)
+y.backward(v)
+print(x.grad)
+
+print(x.requires_grad)
+print((x ** 2).requires_grad)
+
+with torch.no_grad():
+    print((x ** 2).requires_grad)
